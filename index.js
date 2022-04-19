e6490b918fd93efd8d3a0a0d1b38d67b8b9b7853
Basile Carle <basile.carle@edhec.com>
2022-04-19 15:27:45 +0200

    exos réalisé en cours

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express");
-const port = 3000;
+const port = 8080;
 const app = express();
+
+const users = [
+  { id: 1, name: "bob" },
+  { id: 2, name: "patrick" },
+  { id: 3, name: "carlos" },
+];
+
+app.get("/", (req, resp) => {
+  resp.sendStatus(400);
+});
+
+app.get("/users", (req, resp) => {
+  resp.send(users);
+});
+
+app.get("/user/:id", (req, res) => {
+  console.log(req.params);
+  res.send(`tu as demandé l'utilisateur avec l'id = ${req.params.id}`);
+  res.send(`${users[parseInt(req.params.id) - 1].name}`);
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.error("something bad happened");
